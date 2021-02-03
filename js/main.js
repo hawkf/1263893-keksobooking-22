a315da9255ac7523027356feb8cdd8fbe204aa7e
hawkf <fedor.gorokhov@gmail.com>
2021-02-03 18:54:06 +0200

    Внес исправления

--- js/main.js
+++ js/main.js
@@ -1,40 +1,31 @@
 //Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
-  let result;
-
-  if(min < max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-  } else if (min > max) {
-    min  = Math.ceil(max);
-    max = Math.floor(min);
+  if (min > max) {
+    [min, max] = [max, min]
   }
-  result = Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 
-  if(min === max) {
-    result = Math.floor(min);
+  if (min < 0) {
+    // eslint-disable-next-line no-console
+    console.log("Введен не верный диапазон")
   }
-  return result;
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getRandomFloatInclusive(min, max, decimalNumber) {
-  let result;
   if (min > max) {
-    let x = min;
-    min = max;
-    max = x;
+    [min, max] = [max, min]
   }
 
-  result = Math.random() * (max - min + 1) + min;
-
-  if (min === max) {
-    result = min;
+  if (min < 0) {
+    // eslint-disable-next-line no-console
+    console.log("Введен не верный диапазон")
   }
 
-  return  result.toFixed(decimalNumber);
+  return  parseFloat((Math.random() * (max - min + 1) + min).toFixed(decimalNumber));
 }
 
 // eslint-disable-next-line no-console
-console.log(getRandomIntInclusive(1.2332, 50.3234));
+console.log(getRandomIntInclusive(-1.2332, 50.3234));
 // eslint-disable-next-line no-console
 console.log(getRandomFloatInclusive(100.23434, 100.23434, 3));
