a0cddd02164505410d9eea7492f9638f602a38f3
hawkf <fedor.gorokhov@gmail.com>
2021-02-18 01:42:02 +0200

    Выполнил доработки

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,18 @@
-import {getAdvertisements} from './data.js'
 const map = document.querySelector('#map-canvas');
 const advertisementTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
+const typeMap = new Map();
+typeMap.set('flat', 'Квартира');
+typeMap.set('bungalow', 'Бунгало')
+typeMap.set('house', 'Дом');
+typeMap.set('palace', 'Дворец');
 
-const advertisements = getAdvertisements(1);
+//const advertisements = getAdvertisements(1);
 
-const advertisementFragment = document.createDocumentFragment();
+//const advertisementFragment = document.createDocumentFragment();
 
-advertisements.forEach(({author, offer, location}) => {
+/*advertisements.forEach(({author, offer, location}) => {
   const advertisementElement = advertisementTemplate.cloneNode(true);
   advertisementElement.querySelector('.popup__title').textContent = offer.title;
   advertisementElement.querySelector('.popup__text--address').textContent = offer.address;
@@ -27,9 +31,9 @@ advertisements.forEach(({author, offer, location}) => {
     case 'palace':
       type = 'Дворец';
   }
-  advertisementElement.querySelector('.popup__type').textContent = type;
-  advertisementElement.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей'
-  advertisementElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
+  //advertisementElement.querySelector('.popup__type').textContent = type;
+ // advertisementElement.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей'
+  //advertisementElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
   const featuresList = advertisementElement.querySelector('.popup__features');
   featuresList.innerHTML = '';
   for(let i = 0; i < offer.features.length; i++) {
@@ -50,7 +54,48 @@ advertisements.forEach(({author, offer, location}) => {
 
 
   advertisementFragment.appendChild(advertisementElement);
-});
+});*/
+const getHousingName = function(type) {
+  return typeMap.get(type);
+};
 
-map.appendChild(advertisementFragment);
+const createFeaturesItems = function (features) {
+  const featuresFragment = document.createDocumentFragment();
+  features.forEach((feature) => {
+    const featureItem = document.createElement('li');
+    featureItem.className = 'popup__feature popup__feature--' + feature;
+    featuresFragment.appendChild(featureItem);
+  });
+  return featuresFragment;
+}
+const createPhotosElements = function (photos, photoElement) {
+  const photosFragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    photoElement.src = photo;
+    photosFragment.appendChild(photoElement.cloneNode(true));
+  });
+  return photosFragment;
+}
+const makeAdvertisement = function ({author, offer}) {
+  const advertisementElement = advertisementTemplate.cloneNode(true);
+  advertisementElement.querySelector('.popup__title').textContent = offer.title;
+  advertisementElement.querySelector('.popup__text--address').textContent = offer.address;
+  advertisementElement.querySelector('.popup__text--price').textContent = offer.price + ' ₽/ночь';
+  advertisementElement.querySelector('.popup__type').textContent = getHousingName(offer.type);
+  advertisementElement.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей'
+  advertisementElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
+  const featuresList = advertisementElement.querySelector('.popup__features');
+  featuresList.innerHTML = '';
+  featuresList.appendChild(createFeaturesItems(offer.features));
+  advertisementElement.querySelector('.popup__description').textContent = offer.description;
+  const photos= advertisementElement.querySelector('.popup__photos');
+  const photoElement = advertisementElement.querySelector('.popup__photo');
+  photos.removeChild(photoElement);
+  photos.appendChild(createPhotosElements(offer.photos, photoElement));
+  advertisementElement.querySelector('.popup__avatar').src = author.avatar;
+  map.appendChild(advertisementElement);
+};
+
+export {makeAdvertisement};
+
 
